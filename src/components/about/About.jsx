9554784ac7ee12017708ff9a8b6656810b879676
Soliha <solyatursuntasheva@gmail.com>
2023-01-21 00:40:28 +0500

    Add CV to site

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/bg-solya.jpg";
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/CV-Solya.pdf";
 import Info from "./Info";
 
 const About = () => {
