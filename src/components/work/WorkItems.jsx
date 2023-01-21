36e80a6a4722d06f46880a8cf5fe244d2eebe9fc
Soliha <solyatursuntasheva@gmail.com>
2023-01-21 13:12:55 +0500

    Change some styles

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -4,10 +4,16 @@ const WorkItems = ({ item }) => {
   return (
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="portfolio" className="work__img" />
-      <h3 className="work__title">{item.title}</h3>
-      <a href={item.link} target="_blank" className="work__button">
-        Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
-      </a>
+      <span className="card__links">
+        <a href={item.link} target="_blank" className="work__button">
+          <i class="bx bx-world"></i>
+          Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+        </a>
+        <a href={item.link} target="_blank" className="work__button">
+          <i className="uil uil-github-alt"></i>
+          Github <i className="bx bx-right-arrow-alt work__button-icon"></i>
+        </a>
+      </span>
     </div>
   );
 };
