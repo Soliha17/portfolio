49c0a0996b7e8c69e9456480f2eb743a9a691cbd
Soliha <solyatursuntasheva@gmail.com>
2023-01-31 18:54:11 +0500

    Add resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/bg-solya.jpg";
-import CV from "../../assets/CV-Solya.pdf";
+import CV from "../../assets/solya-resume.pdf";
 import Info from "./Info";
 
 const About = () => {
